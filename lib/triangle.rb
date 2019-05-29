952ae62e3a4c46a48fdf063ff4b9e9b54926f30c
elijahmm17 <elijahmm17@gmail.com>
2019-05-29 05:12:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,21 @@ class Triangle
   end 
   
   def kind
-    same_sides_1 = []
+    same_sides_1 = [@side_1]
     same_sides_2 = []
-    
+    @side_1 == @side_2 ? same_sides_1 << @side_2 : same_sides_2 << @side_2 
+    if @side_3 == @side_2 
+      same_sides_2 << @side_3
+    elsif @side_3 == @side_1 
+      same_sides_1 << @side_3 
+    end 
+    if same_sides_1.count == 3 
+      :equilateral 
+    elsif same_sides_1.count == 2 or same_sides_2.count == 2 
+      :isosceles 
+    else 
+      :scalene 
+    end 
   end 
   
   def TriangleError < StandardError
